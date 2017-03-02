c0214140d4efa315388b183154afd6ffa8c448ac
Jan Meisel <janmeisel@yahoo.de>
2017-03-02 08:19:07 +0100

    remove staticmethod

--- gamestate.py
+++ gamestate.py
@@ -1,6 +1,3 @@
-import pygame
-
-
 class Gamestate:
     def __init__(self):
         pass
@@ -13,19 +10,3 @@ class Gamestate:
 
     def close(self, gsh):
         pass
-
-    @staticmethod
-    def handle_events(gsh):
-        other_events = []
-        events = []
-        if gsh.is_sleeping:
-            events.append(pygame.event.wait())
-        else:
-            events = pygame.event.get()
-
-        for event in events:
-            if event.type == pygame.QUIT:
-                gsh.close_all()
-            else:
-                other_events.append(event)
-        return other_events
