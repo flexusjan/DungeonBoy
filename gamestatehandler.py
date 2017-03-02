97e0c1eec41912ac78bc4e0d1f5c71d076569b72
Jan Meisel <janmeisel@yahoo.de>
2017-03-02 08:24:50 +0100

    make self.states protected

--- gamestatehandler.py
+++ gamestatehandler.py
@@ -1,24 +1,24 @@
 class Gamestatehandler:
     def __init__(self):
-        self.states = []
+        self._states = []
 
     def init_state(self):
-        if self.states:
-            self.states[-1].init(self)
+        if self._states:
+            self._states[-1].init(self)
 
     def update_state(self):
-        if self.states:
-            self.states[-1].update(self)
+        if self._states:
+            self._states[-1].update(self)
             return True
         else:
             return False
 
     def close_state(self):
-        if self.states:
-            self.states[-1].close(self)
+        if self._states:
+            self._states[-1].close(self)
 
     def close_all(self):
-        while self.states:
+        while self._states:
             self.pop_state()
 
     def switch_state(self, state):
@@ -26,9 +26,9 @@ class Gamestatehandler:
         self.push_state(state)
 
     def push_state(self, state):
-        self.states.append(state)
+        self._states.append(state)
         self.init_state()
 
     def pop_state(self):
         self.close_state()
-        self.states.pop()
+        self._states.pop()
