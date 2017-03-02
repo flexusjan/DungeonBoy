506ef79801a8ccd80d16106e7e590b2b481d14fd
Jan Meisel <janmeisel@yahoo.de>
2017-03-02 08:17:11 +0100

    check for pygame.QUIT event

--- gamestateLvl01.py
+++ gamestateLvl01.py
@@ -1,5 +1,6 @@
-from gamestate import *
+from gamestate import Gamestate
 from spritehandler import Spritehandler
+import pygame
 import tools
 import os
 
@@ -10,14 +11,19 @@ class GamestateLvl01(Gamestate):
         self.spritehandler = Spritehandler()
 
     def init(self, gsh):
-        sprites = tools.load_tmx_map(os.path.join("media", "sprites", "static", "lvl01.tmx"), 48)
+        sprites = tools.load_tmx_map(os.path.join("media", "sprites", "static", "lvl01.tmx"), 32
+                                     )
         self.spritehandler.add(True, *sprites)
-        # gsh.offset_x, gsh.offset_y = -800, -800
+        gsh.offset_x, gsh.offset_y = 1000, 1000
 
     def update(self, gsh):
-        for event in gsh.events:
+        for event in pygame.event.get():
             # Escape Key
             if event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
+                gsh.close_all()
+                return
+            # Exit Button
+            elif event.type == pygame.QUIT:
                 gsh.close_all()
                 return
 
