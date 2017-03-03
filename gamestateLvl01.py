0dcb473c01c8cdb9d6cd25072f08420cdc26f875
Jan Meisel <janmeisel@yahoo.de>
2017-03-03 08:56:33 +0100

    store unused events in gsh.events store pressed keys in gsh.pressed_key calc delta time

--- gamestateLvl01.py
+++ gamestateLvl01.py
@@ -16,7 +16,12 @@ class GamestateLvl01(Gamestate):
         self.spritehandler.add(True, *sprites)
         gsh.offset_x, gsh.offset_y = 1000, 1000
 
+        # load animated sprites
+        self.spritehandler.add(False, gsh.sprites['DungeonBoy'])
+
     def update(self, gsh):
+        # handle global events, store remainder in gsh.events
+        gsh.events[:] = []
         for event in pygame.event.get():
             # Escape Key
             if event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
@@ -26,11 +31,19 @@ class GamestateLvl01(Gamestate):
             elif event.type == pygame.QUIT:
                 gsh.close_all()
                 return
+            else:
+                gsh.events.append(event)
+        gsh.pressed_keys = pygame.key.get_pressed()
 
-        # gsh.offset_x += 1
-        # gsh.offset_y += 1
+        # calculate time delta
+        gsh.time_delta = pygame.time.get_ticks() - gsh.time
+        gsh.time = pygame.time.get_ticks()
+
+        gsh.offset_x = gsh.sprites['DungeonBoy'].x - gsh.screen_w/2
+        gsh.offset_y = gsh.sprites['DungeonBoy'].y - gsh.screen_h/2
+
         gsh.screen.fill((0, 0, 0))
-        self.spritehandler.update()
+        self.spritehandler.update(gsh)
         self.spritehandler.draw(gsh.screen, (gsh.offset_x, gsh.offset_y))
         pygame.display.flip()
         gsh.clock.tick(0)
