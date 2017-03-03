bbb21cd35383ed06922894af918b2ad401100d71
Jan Meisel <janmeisel@yahoo.de>
2017-03-03 10:58:02 +0100

    change dict creation

--- gamestateInit.py
+++ gamestateInit.py
@@ -33,26 +33,23 @@ class GamestateInit(Gamestate):
 
 def load_dungeonboy(gsh):
     image = pygame.image.load(os.path.join("media", "sprites", "animated", "dungeonboy_2.png"))
-    animations = {}
-    animations['SpellcastUp'] = load_animation(image, (0, 0), (64, 64), 7, [150] * 7)
-    animations['SpellcastLeft'] = load_animation(image, (0, 64), (64, 64), 7, [150] * 7)
-    animations['SpellcastDown'] = load_animation(image, (0, 128), (64, 64), 7, [150] * 7)
-    animations['SpellcastRight'] = load_animation(image, (0, 192), (64, 64), 7, [150] * 7)
-
-    animations['IdleUp'] = load_animation(image, (0, 512), (64, 64), 1, [0])
-    animations['IdleLeft'] = load_animation(image, (0, 576), (64, 64), 1, [0])
-    animations['IdleDown'] = load_animation(image, (0, 640), (64, 64), 1, [0])
-    animations['IdleRight'] = load_animation(image, (0, 704), (64, 64), 1, [0])
-
-    animations['WalkUp'] = load_animation(image, (64, 512), (64, 64), 8, [80] * 8)
-    animations['WalkLeft'] = load_animation(image, (64, 576), (64, 64), 8, [80] * 8)
-    animations['WalkDown'] = load_animation(image, (64, 640), (64, 64), 8, [80] * 8)
-    animations['WalkRight'] = load_animation(image, (64, 704), (64, 64), 8, [80] * 8)
-    animations['Hurt'] = load_animation(image, (0, 1280), (64, 64), 6, [100] * 6, 0, False)
-    animations['AttackUp'] = load_animation(image, (0, 1344), (192, 192), 6, [75] * 6)
-    animations['AttackLeft'] = load_animation(image, (0, 1536), (192, 192), 6, [75] * 6)
-    animations['AttackDown'] = load_animation(image, (0, 1728), (192, 192), 6, [75] * 6)
-    animations['AttackRight'] = load_animation(image, (0, 1920), (192, 192), 6, [75] * 6)
+    animations = {'SpellcastUp': load_animation(image, (0, 0), (64, 64), 7, [150] * 7),
+                  'SpellcastLeft': load_animation(image, (0, 64), (64, 64), 7, [150] * 7),
+                  'SpellcastDown': load_animation(image, (0, 128), (64, 64), 7, [150] * 7),
+                  'SpellcastRight': load_animation(image, (0, 192), (64, 64), 7, [150] * 7),
+                  'IdleUp': load_animation(image, (0, 512), (64, 64), 1, [0]),
+                  'IdleLeft': load_animation(image, (0, 576), (64, 64), 1, [0]),
+                  'IdleDown': load_animation(image, (0, 640), (64, 64), 1, [0]),
+                  'IdleRight': load_animation(image, (0, 704), (64, 64), 1, [0]),
+                  'WalkUp': load_animation(image, (64, 512), (64, 64), 8, [80] * 8),
+                  'WalkLeft': load_animation(image, (64, 576), (64, 64), 8, [80] * 8),
+                  'WalkDown': load_animation(image, (64, 640), (64, 64), 8, [80] * 8),
+                  'WalkRight': load_animation(image, (64, 704), (64, 64), 8, [80] * 8),
+                  'Hurt': load_animation(image, (0, 1280), (64, 64), 6, [100] * 6, 0, False),
+                  'AttackUp': load_animation(image, (0, 1344), (192, 192), 6, [75] * 6),
+                  'AttackLeft': load_animation(image, (0, 1536), (192, 192), 6, [75] * 6),
+                  'AttackDown': load_animation(image, (0, 1728), (192, 192), 6, [75] * 6),
+                  'AttackRight': load_animation(image, (0, 1920), (192, 192), 6, [75] * 6)}
 
     gsh.sprites['DungeonBoy'] = Animatedsprite(animations, 'IdleDown', func_dungeonboy, 1500, 1500, 50, True)
     gsh.sprites['DungeonBoy'].direction = 'Down'
